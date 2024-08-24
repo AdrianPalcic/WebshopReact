66f685aa16601ff31a34391d9392767e683a3d0b
Adrian Palcic <adrian.palcic@gmail.com>
2024-08-24 20:59:04 +0200

    final look of homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,7 @@
 import "../Home-page.css";
-import image from "../assets/images/hero2.jpeg";
+import "@fortawesome/react-fontawesome";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import cart from "../assets/images/shopping_cart_24dp_088178_FILL0_wght400_GRAD0_opsz24.svg"
 import sectionImage1 from "../assets/images/feature_section_1.png";
 import sectionImage2 from "../assets/images/feature_section_2.png";
 import sectionImage3 from "../assets/images/feature_section_3.png";
@@ -13,8 +15,12 @@ import productImage5 from "../assets/images/clothes5.jpg";
 import productImage6 from "../assets/images/clothes6.jpg";
 import productImage7 from "../assets/images/clothes7.jpg";
 import productImage8 from "../assets/images/clothes8.jpg";
-import productImage9 from "../assets/images/clothes9.jpg";
-import banner from "../assets/images/banner.avif"
+import banner from "../assets/images/banner.avif";
+import summer from "../assets/images/summer.jpg"
+import logo from "../assets/images/brand.logo.png"
+import payPic from "../assets/images/pay.png";
+import AppStore from "../assets/images/appstore.jpg";
+import TrgPlay from "../assets/images/trgplay.jpg";
 function Home() {
 
     const fetatureData = [
@@ -118,6 +124,9 @@ function Home() {
     const bannerBackground = {
         backgroundImage: `url(${banner})`,
     }
+    const bannerImageBackground = {
+        backgroundImage: `url(${summer})`
+    }
     return (
       <>
 <section class="hero-section" style={heroBackground}>
@@ -152,7 +161,7 @@ function Home() {
         })}
 </section>
 <section className="featured-products">
-    <h2>Featured Products</h2>
+    <h2>Istaknuti Proizvodi</h2>
     <p>Some text with 10 long words</p>
     <div className="prod-box">
         {featuredProductData.map((product) => {
@@ -174,7 +183,7 @@ function Home() {
                    </div>
                    <p>{product.price}</p>
                </div>
-               <a href="#"><i className="fal-fa-shopping-cart"></i></a>
+               <a href="#"><i class="cart-btn" ><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#088178"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/></svg></i></a>
            </div>
             )
         })}
@@ -186,7 +195,133 @@ function Home() {
          <h1>DO <span>70% Popusta</span> na majice i dodatke</h1>
         <button className="banner-explore">Pogledaj više</button>
 </section>
-<section>NewArrivals</section>
+<section className="featured-products novo">
+    <h2>Novo!</h2>
+    <p>Some text with 10 long words</p>
+    <div className="prod-box">
+        {featuredProductData.map((product) => {
+            return (
+               <div className="pro" key={product.id}>
+                
+               <img src={product.img}></img>
+               <div className="desc">
+                   <span>
+                       {product.made}
+                   </span>
+                   <h4>{product.Title}</h4>
+                   <div className="star">
+                       <i class="fa fa-star"></i>
+                       <i class="fa fa-star"></i>
+                       <i class="fa fa-star"></i>
+                       <i class="fa fa-star"></i>
+                       <i class="fa fa-star"></i>
+                   </div>
+                   <p>{product.price}</p>
+               </div>
+               <a href="#"><i class="cart-btn" ><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#088178"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/></svg></i></a>
+           </div>
+            )
+        })}
+        </div>
+     
+</section>
+<section className="sec-pad bimbo">
+    <div className="banner-box" style={bannerImageBackground}>
+    <h2>Lorem ipsum</h2>
+    <h1>Lorem ipsum dolor sit</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+    <button className="box-btn">Pogledaj</button>
+    </div>
+
+    <div className="banner-box" style={bannerImageBackground}>
+    <h2>Lorem ipsum</h2>
+    <h1>Lorem ipsum dolor sit</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+    <button className="box-btn">Pogledaj</button>
+    </div>
+</section>
+<section className=" bombi">
+    <div className="banner-box small-box" style={bannerImageBackground}>
+    <h2>Lorem ipsum</h2>
+    
+    <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>
+    </div>
+
+    <div className="banner-box small-box" style={bannerImageBackground}>
+    <h2>Lorem ipsum</h2>
+  
+    <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>
+    </div>
+
+    <div className="banner-box small-box" style={bannerImageBackground}>
+    <h2>Lorem ipsum</h2>
+
+    <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>
+    </div>
+</section>
+<section className="comeandvisit sec-pad">
+    <div className="posjeti-text">
+        <h2>Posjeti nas</h2>
+        <p>Dođi uživo pregledati <span>artikle</span></p>
+    </div>
+    <div className="adresa">
+        <i><svg xmlns="http://www.w3.org/2000/svg" height="100px" viewBox="0 -960 960 960" width="100px" fill="#BB271A">
+    <path d="M480.09-490q28.91 0 49.41-20.59 20.5-20.59 20.5-49.5t-20.59-49.41q-20.59-20.5-49.5-20.5t-49.41 20.59q-20.5 20.59-20.5 49.5t20.59 49.41q20.59 20.5 49.5 20.5ZM480-159q133-121 196.5-219.5T740-552q0-117.79-75.29-192.9Q589.42-820 480-820t-184.71 75.1Q220-669.79 220-552q0 75 65 173.5T480-159Zm0 79Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z"/>
+</svg></i>
+        <input type="text" disabled  placeholder="Draškovićeva ul. 44, 10000, Zagreb"/>
+    </div>
+</section>
+<footer className="sec-pad">
+    <div className="col">
+    <img className="logo" src={logo} alt="brand_logo" />
+    <h2>Kontakt</h2>
+    <p><strong>Adresa:</strong> Draškovićeva ul. 44, 10000, Zagreb</p>
+    <p><strong>Mobitel:</strong>0957482199</p>
+    <p><strong>Sati:</strong> 06:00 - 24:00 Pon - Ned </p>
+    <div className="follow">
+        <h4>Pratite nas</h4>
+        <div className="icon">
+        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-x-twitter"></i>
+        <i class="fa-brands fa-pinterest"></i>
+        </div>
+    </div>
+    </div>
+
+    <div className="col">
+        <h2>Informacije</h2>
+        <a href="#">O nama</a>
+        <a href="#">Dostava</a>
+        <a href="#">Uvjeti i Odredbe</a>
+        <a href="#">Kako Štitimo Vašu Privatnost</a>
+        <a href="#">Kontaktiraj nas</a>
+    </div>
+
+    <div className="col">
+        <h2>Moj Racun</h2>
+        <a href="#">Login</a>
+        <a href="#">Kolica</a>
+        <a href="#">Lista želja</a>
+        <a href="#">Prati moj paket</a>
+        <a href="#">Pomoc</a>
+    </div>
+
+    <div className="col npm-i">
+        <h2>Instaliraj aplikaciju</h2>
+        <p>Sa App storea ili Google Play</p>
+        <div className="row">
+            <img src={AppStore} alt="App Store"></img>
+            <img src={TrgPlay} alt="Google Store"></img>
+        </div>
+        <p>Nacini placanja</p>
+        <img src={payPic} alt="Nacini placanja"></img>
+    </div>
+
+    <div className="cr">
+        <p>&copy; 2024. All rights reserved.</p>
+    </div>
+</footer>
 </>
     )
 }
